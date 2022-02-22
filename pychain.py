0b634a7d7e455bc4068bd3bff57d4322341eede7
PFCorzo <pfcorzo@outlook.com>
2022-02-22 10:32:57 -0500

    Not done

---
+++ pychain.py
@@ -0,0 +1,87 @@
+# BlockChain on Streamlit 
+
+## Import Dependencies
+import streamlit as st
+from dataclasses import dataclass
+from typing import Any, List
+import datetime as datetime
+import pandas as pd
+import hashlib
+
+## Create a record Data Class that consists of the send, reciver, and amount 
+@dataclass 
+class Record():
+    sender: str
+    receiver: str
+    amount: float
+
+@dataclass 
+class Block: 
+    record: Record 
+    creator_id: int 
+    prev_hash: str = "0"
+    timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S") 
+    nonce: int = 0
+
+    def hash_block(self): 
+        sha = hashlib.sha256()
+        record = str(self.record).encode
+        sha.update(record) 
+        creator_id = str(self.creator_id).encode() 
+        sha.update(creator_id) 
+        timestamp = str(self.timestamp).encode()
+        sha.update(timestamp) 
+        prev_hash = str(self.prev_hash).encode()
+        sha.update(prev_hash) 
+        nonce = str(self.nonce).encode()
+        sha.update(nonce) 
+
+        return sha.hexdigest() 
+    @dataclass
+    class Pychain: 
+        chain: list[Block] 
+        difficulty: int = 4
+
+        def proof_of_work(self, block): 
+            calculated_hash = block_block() 
+            num_of_zeroes = "0" * self.difficulty 
+            while not calculated_hash.startwith(num_of_zeroes): 
+                block.nonce +=1
+                calculated_hash = block.hash_block() 
+            print("Hash winner", calculated_hash) 
+            return block 
+        
+        def add_block(self, candidate_block): 
+            block = self.proof_of_work(candidate_block) 
+            self.chain += [block]
+
+        def is_valid(self): 
+            block_hash = self.chain[0].hash_block() 
+            
+            for block in self.chain[1:]:
+                if block_hash != block.prev_hash:
+                    print("Blockchain not valid") 
+                    return False
+                block_hash = block.hash_block()
+            print("Blockhain is valid") 
+            return True 
+
+## Streamlit 
+@st.cache(allow_output_mutation=True)
+def setup():
+        print("Initializing Chain")
+            return PyChain([Block("Genesis", 0)])
+
+st.markdown("# PyChain")
+st.markdown("## Store a Transaction Record in the PyChain")
+pychain = setup()
+sender = st.text_input("sender")
+recipient = st.text_input("recipient")
+amount = st.text_input("amount")
+
+if st.button("Add Block"):
+        prev_block = pychain.chain[-1]
+        prev_block_hash = prev_block.hash_block()
+        new_block = Block(record = Record(sender, recipient, amount),creator_id=42,prev_hash=prev_block_hash)
+        pychain.add_block(new_block) 
+        st.balloons()
